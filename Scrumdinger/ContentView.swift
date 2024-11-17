89e91dc6d3a4034cf676d2ae434498c1a4e50209
wakebro <wakebro119@naver.com>
2024-11-17 01:49:07 -0500

    Initial Commit

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by 형기상 on 11/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 형기상 on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
